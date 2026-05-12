a949ba44d5a3ad7d915046e58dac197c0bd831f8
imihirjain <jmanav251@gmail.com>
2026-05-12 21:55:34 +0530

    footer logo update and add toast and submitting logic

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,29 +1,47 @@
 import { FaRegCopyright } from "react-icons/fa";
 import React from "react";
-
+import { IoIosMail } from "react-icons/io";
+import { IoLogoWhatsapp } from "react-icons/io";
+import { ToastContainer } from "react-toastify";
 const Footer = () => {
   return (
-    <div className="text-center font-man bg-beige text-darkBlue font-bold sm:flex items-center justify-between p-10">
-      <p className="flex items-center lg:gap-2">
-        <FaRegCopyright /> 2026 Aurex CPA. All rights reserved.
-      </p>
+    <>
+      <div className="text-center font-man bg-beige text-darkBlue font-bold sm:flex items-center justify-between p-10">
+        <p className="flex items-center lg:gap-2">
+          <FaRegCopyright /> 2026 Aurex CPA. All rights reserved.
+        </p>
 
-      <ul className="flex flex-col lg:flex-row items-center justify-center gap-2 sm:gap-10 mt-4 sm:mt-0">
-        <li>
-          <a href="mailto:info@aurexcpas.com">info@aurexcpas.com</a>
-        </li>
+        <ul className="flex flex-col lg:flex-row items-center justify-center gap-2 sm:gap-10 mt-4 sm:mt-0">
+          <li>
+            <a
+              className="flex gap-2 items-center"
+              href="mailto:info@aurexcpas.com"
+            >
+              <IoIosMail
+                size={24}
+                className="cursor-pointer hover:text-heading hover:scale-95 transition-all duration-200"
+              />
+              info@aurexcpas.com
+            </a>
+          </li>
 
-        <li>
-          <a
-            href="https://wa.me/919096019133"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            +91 9096019133
-          </a>
-        </li>
-      </ul>
-    </div>
+          <li>
+            <a
+              href="https://wa.me/919096019133"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="flex gap-2 items-center"
+            >
+              <IoLogoWhatsapp
+                size={24}
+                className="cursor-pointer hover:text-heading hover:scale-95 transition-all duration-200"
+              />
+              +91 9096019133
+            </a>
+          </li>
+        </ul>
+      </div>
+    </>
   );
 };
 
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,5 +1,5 @@
-import React, { useRef } from "react";
-import { toast } from "react-toastify";
+import React, { useRef, useState } from "react";
+import { toast, ToastContainer } from "react-toastify";
 import { FaUser } from "react-icons/fa";
 import { MdEmail } from "react-icons/md";
 import { FaMessage } from "react-icons/fa6";
@@ -9,11 +9,12 @@ import emailjs from "@emailjs/browser";
 
 const Form = () => {
   const form = useRef();
+  const [loading, setLoading] = useState(false);
   console.log(import.meta.env.VITE_EMAILJS_SERVICE_ID);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-
+    setLoading(true);
     emailjs
       .sendForm(
         import.meta.env.VITE_EMAILJS_SERVICE_ID,
@@ -22,7 +23,7 @@ const Form = () => {
         import.meta.env.VITE_EMAILJS_PUBLIC_KEY,
       )
       .then(() => {
-        toast.success("Thankyou for contacting", {
+        toast("Thankyou for contacting", {
           position: "top-right",
           autoClose: 3000,
         });
@@ -31,114 +32,119 @@ const Form = () => {
       .catch((err) => {
         console.log(err);
         toast.error("Failed to send");
+      })
+      .finally(() => {
+        setLoading(false);
       });
   };
   return (
-    <form ref={form} onSubmit={handleSubmit}>
-      <div className="p-5 sm:p-8 lg:p-10 flex flex-col gap-6 lg:gap-8 font-man text-beige">
-        {/* row inputs */}
-        <div className="flex flex-col sm:flex-row gap-4 sm:gap-6 lg:gap-10 w-full">
+    <>
+      <ToastContainer position="bottom-right" />
+
+      <form ref={form} onSubmit={handleSubmit}>
+        <div className="p-5 sm:p-8 lg:p-10 flex flex-col gap-6 lg:gap-8 font-man text-beige">
+          {/* row inputs */}
+          <div className="flex flex-col sm:flex-row gap-4 sm:gap-6 lg:gap-10 w-full">
+            <div className="flex flex-col gap-2 w-full">
+              <label
+                htmlFor="username"
+                className="flex items-center gap-3 text-sm sm:text-base"
+              >
+                <FaUser /> Your Name
+              </label>
+              <input
+                name="name"
+                type="text"
+                id="username"
+                className="w-full px-3 sm:px-4 py-2 border-b border-beige
+                         focus:outline-none focus:ring-0"
+              />
+            </div>
+
+            <div className="flex flex-col gap-2 w-full">
+              <label
+                htmlFor="email"
+                className="flex items-center gap-3 text-sm sm:text-base"
+              >
+                <MdEmail />
+                Your email
+              </label>
+
+              <input
+                type="email"
+                name="email"
+                required
+                id="email"
+                className="w-full px-3 sm:px-4 py-2 border-b border-beige 
+                         focus:outline-none focus:ring-0"
+              />
+            </div>
+          </div>
+
+          <div className="flex flex-col sm:flex-row gap-4 sm:gap-6 lg:gap-10 w-full">
+            <div className="flex flex-col gap-2 w-full">
+              <label
+                htmlFor="phone"
+                className="flex items-center gap-3 text-sm sm:text-base"
+              >
+                <FaPhoneAlt /> Your Phone
+              </label>
+              <input
+                type="number"
+                name="phone"
+                id="phone"
+                className="w-full px-3 sm:px-4 py-2 border-b border-beige
+                         focus:outline-none focus:ring-0"
+              />
+            </div>
+
+            <div className="flex flex-col gap-2 w-full">
+              <label
+                htmlFor="location"
+                className="flex items-center gap-3 text-sm sm:text-base"
+              >
+                <FaLocationDot />
+                Your Location
+              </label>
+
+              <input
+                type="text"
+                name="location"
+                id="location"
+                className="w-full px-3 sm:px-4 py-2 border-b border-beige 
+                         focus:outline-none focus:ring-0"
+              />
+            </div>
+          </div>
+
+          {/* message */}
           <div className="flex flex-col gap-2 w-full">
             <label
-              htmlFor="username"
+              htmlFor="message"
               className="flex items-center gap-3 text-sm sm:text-base"
             >
-              <FaUser /> Your Name
+              <FaMessage />
+              Leave us a message
             </label>
-            <input
-              name="name"
-              type="text"
-              id="username"
+
+            <textarea
+              id="message"
+              name="message"
+              rows={4}
               className="w-full px-3 sm:px-4 py-2 border-b border-beige
-                         focus:outline-none focus:ring-0"
-            />
-          </div>
-
-          <div className="flex flex-col gap-2 w-full">
-            <label
-              htmlFor="email"
-              className="flex items-center gap-3 text-sm sm:text-base"
-            >
-              <MdEmail />
-              Your email
-            </label>
-
-            <input
-              type="email"
-              name="email"
-              required
-              id="email"
-              className="w-full px-3 sm:px-4 py-2 border-b border-beige 
-                         focus:outline-none focus:ring-0"
-            />
-          </div>
-        </div>
-
-        <div className="flex flex-col sm:flex-row gap-4 sm:gap-6 lg:gap-10 w-full">
-          <div className="flex flex-col gap-2 w-full">
-            <label
-              htmlFor="phone"
-              className="flex items-center gap-3 text-sm sm:text-base"
-            >
-              <FaPhoneAlt /> Your Phone
-            </label>
-            <input
-              type="number"
-              name="number"
-              id="phone"
-              className="w-full px-3 sm:px-4 py-2 border-b border-beige
-                         focus:outline-none focus:ring-0"
-            />
-          </div>
-
-          <div className="flex flex-col gap-2 w-full">
-            <label
-              htmlFor="location"
-              className="flex items-center gap-3 text-sm sm:text-base"
-            >
-              <FaLocationDot />
-              Your Location
-            </label>
-
-            <input
-              type="text"
-              name="location"
-              id="location"
-              className="w-full px-3 sm:px-4 py-2 border-b border-beige 
-                         focus:outline-none focus:ring-0"
-            />
-          </div>
-        </div>
-
-        {/* message */}
-        <div className="flex flex-col gap-2 w-full">
-          <label
-            htmlFor="message"
-            className="flex items-center gap-3 text-sm sm:text-base"
-          >
-            <FaMessage />
-            Leave us a message
-          </label>
-
-          <textarea
-            id="message"
-            name="message"
-            rows={4}
-            className="w-full px-3 sm:px-4 py-2 border-b border-beige
                        focus:outline-none focus:ring-0 resize-none"
-          ></textarea>
-        </div>
+            ></textarea>
+          </div>
 
-        {/* button */}
-        <button
-          className="w-full sm:w-fit cursor-pointer font-bold text-base sm:text-lg 
-                           px-4 py-2 text-darkBlue active:scale-95 
-                           rounded bg-lightBrown"
-        >
-          Submit
-        </button>
-      </div>
-    </form>
+          {/* button */}
+          <button
+            className={`w-full sm:w-fit transition font-bold text-base sm:text-lg px-4 py-2 text-darkBlue  rounded bg-lightBrown ${loading ? "opacity-70 cursor-not-allowed" : "cursor-pointer active:scale-95"}`}
+          >
+            {loading ? "Submitting..." : "Submit"}
+          </button>
+        </div>
+      </form>
+    </>
   );
 };
 
